c7a92d827560acde58674e466e3538b295315539
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-02-12 12:35:10 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -23,9 +23,13 @@
 	<link rel="canonical" href=".">
 	<link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
         <script async src="https://cdn.ampproject.org/v0.js"></script>
+	<script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>
         <!-- Import other AMP Extensions here -->
         <style amp-custom>
         /* Add your styles here */
         </style>
 	<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 </head>
+<body>
+	<?= $Wcms->settings() ?>
+	<?= $Wcms->alerts() ?>
