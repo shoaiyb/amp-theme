44e81dcded6c75456e90b3dabf605243208e886d
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-02-12 11:47:25 +0100

    Create theme.php

---
+++ theme.php
@@ -0,0 +1,27 @@
+<?php global $Wcms ?>
+
+<!doctype html>
+<html ⚡ lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Meta -->
+  <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+	<meta name="description" content="<?= $Wcms->page('description') ?>">
+	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
+  <!-- Open Graph -->
+	<meta property="og:url" content="<?= $this->url() ?>" />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
+	<meta property="og:title" content="<?= $Wcms->page('title') ?>" />
+	<meta name="twitter:site" content="<?= $this->url() ?>" />
+	<meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
+	<meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
+
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<?= $Wcms->css()?>
+
+	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css')?>">
+</head>
