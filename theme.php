5a842fc647727140b56fb94caddfc6b34befe1ce
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-02-12 12:12:52 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -6,11 +6,11 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Meta -->
-  <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+        <!-- Meta -->
+        <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
 	<meta name="description" content="<?= $Wcms->page('description') ?>">
 	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
-  <!-- Open Graph -->
+        <!-- Open Graph -->
 	<meta property="og:url" content="<?= $this->url() ?>" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
@@ -21,9 +21,10 @@
 
 
 	<link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
     <!-- Import other AMP Extensions here -->
-    <style amp-custom>
+        <style amp-custom>
     /* Add your styles here */
-    </style>
+        </style>
+	<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 </head>
